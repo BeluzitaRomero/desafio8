aea4c0464ca8db8b9675b3b834594c673943afcb
Belen Romero <romero.abelen@gmail.com>
2022-04-09 18:31:00 -0300

    server.js solo con routes

--- index.js
+++ index.js
@@ -11,36 +11,6 @@ app.use(express.json());
 const productsRouter = require("./router/productRoutes");
 app.use("/api/products", productsRouter);
 
-//READ
-// app.get("/all", async (req, res) => {
-//   res.send(await container.getAll());
-// });
-
-// app.get("/get/:id", async (req, res) => {
-//   res.status(200).send(await container.getById(req.params.id));
-// });
-
-//CREATE
-// app.post("/", async (req, res) => {
-//   const product = {
-//     title: req.body.title,
-//     price: req.body.price,
-//     img: req.body.img,
-//   };
-
-//   res.status(200).send(await container.save(product));
-// });
-
-//UPDATE
-// app.put("/update/:id", async (req, res) => {
-//   res.status(200).send(await container.updateProduct(req.body, req.params.id));
-// });
-
-//DELETE
-// app.delete("/del/:id", async (req, res) => {
-//   res.status(200).send(await container.deleteById(req.params.id));
-// });
-
 app.listen(8080, () => {
   console.log("Server run on port 8080");
 });
